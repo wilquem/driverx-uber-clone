a3badf15017f78b627cb7a834af037f8bd9c6b46
wilquem <emanuel_wilquem098@hotmail.com>
2022-07-27 11:05:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import Login from './src/pages/Login'
+
+AppRegistry.registerComponent(appName, () => Login);
---
+++ src/styles/index.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+import theme from './theme.json';
+
+export const Container = styled.View`
+    flex: 1;
+    background-color:${(props) => props.color ? theme.colors[props.color] : 'transparent'};
+    flex-direction: ${(props) => props.row ? 'row' : 'column'};
+    justify-content: ${(props) => props.justify || 'center'};
+    padding: ${(props) => props.padding || 0}px;
+    width:100%;
+    align-items:${(props) => props.align || 'center'};
+    max-width: ${(props) => props.width || '100%'};
+    max-height: ${(props) => props.height  ? props.height + 'px' : 'auto'};
+    position: ${(props) => props.position || 'relative'};
+    top:${(props) => props.top || 0};
+    z-index:${(props) => props.zIndex || 1};
+`;
+
+export const Button = styled.TouchableOpacity`
+    width:100%;
+    height: ${(props) => props.height || 50};
+    padding: ${(props) => props.compact ? 5 : 15}px;
+    margin:${(props) => props.margin ? 5 : 10}px;
+    opacity: ${(props) => props.disabled ? 0.5 : 1};
+    border: 0.5px solid #000;
+    flex-direction: ${(props) => props.column ? 'column' : 'row'};
+    align-items:center;
+    justify-content:space-between;
+
+`;
+
+export const ButtonText = styled.Text`
+    color:${props => props.color ? theme.colors[props.color] : '#000' };
+    font-weight: ${props => props.weight || 'normal'};
+`
